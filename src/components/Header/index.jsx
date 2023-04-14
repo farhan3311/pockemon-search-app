088b884ba4414fd210705aa4fcf72322bc53663b
farhan3311 <farhansidat3311@gmail.com>
2023-04-14 13:05:55 +0100

    Final commit 2

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
+const style = ({isActive}) => ({color: isActive ? '#FFC300' : ""})
 
 export default function Header() {
   return (
     <main>
       <header className='header'>
         <nav className='nav'>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/search">Search</NavLink>
-            <NavLink to="/Saved">Saved</NavLink>
+            <NavLink style={style} to="/">Home</NavLink>
+            <NavLink style={style} to="/search">Search</NavLink>
+            <NavLink style={style} to="/Saved">Saved</NavLink>
         </nav>
         <Outlet />
       </header>
